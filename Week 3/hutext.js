911eca5626b10403bb0b5f564a35f9deef50fe76
carmen-0309 <ccedano@my.harrisburgu.edu>
2025-01-29 18:17:22 -0500

    Week 3 work finished

---
+++ Week 3/hutext.js
@@ -0,0 +1,10 @@
+function setup () {
+    createCanvas(500, 300);
+    background(224, 150, 255);
+    textSize(32);
+    textStyle(BOLDITALIC);
+    text("Harrisburg University", 100, 50);
+    textSize(14);
+    text("My name is Carmen and I go to HU. My major is IMED and this is", 20, 100);
+    text(" my second semester. I plan to graduate in or before 2028.", 20, 120);
+};
